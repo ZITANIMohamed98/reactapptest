e633f8083f0676d3ea8f68f50cf3df2bfbb8975a
ZITANIMohamed98 <zitaniceo@gmail.com>
2025-04-22 09:39:31 +0000

    fixing the request content

--- api/function_app.py
+++ api/function_app.py
@@ -8,7 +8,7 @@ app = func.FunctionApp()
 @app.route(route="nothingtrigger", auth_level=func.AuthLevel.ANONYMOUS)
 def nothingtrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    datetime_now = datetime.datetime.now()  
     name = req.params.get('name')
     if not name:
         try:
@@ -19,7 +19,7 @@ def nothingtrigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}, you made a request at .", status_code=200)
+        return func.HttpResponse(f"Hello, {name}, you made a request at {datetime_now}.", status_code=200)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
